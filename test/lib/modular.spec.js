e83efc05e4545c01b9af7840a09618ee5bb1bf2b
Johnny Estilles <johnny.estilles@agentia.asia>
2015-04-12 01:51:08 +0800

    test(core): added tests for Modular class

---
+++ test/lib/modular.spec.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var Modular = require('../../lib/modular');
+
+describe('Modular()', function() {
+  var modular = new Modular();
+
+  it('should be a function', function() {
+    expect(Modular).to.be.a('function');
+  });
+
+  it('should be a factory of Modular objects', function() {
+    expect(modular).to.be.an.instanceof(Modular);
+  });
+
+  describe('API', function() {
+
+    it('should expose .registerFunction()', function() {
+      expect(modular).to.have.property('registerFunction').that.is.a('function');
+    });
+
+    it('should expose .registerModule()', function() {
+      expect(modular).to.have.property('registerModule').that.is.a('function');
+    });
+
+    it('should expose .registerFolder()', function() {
+      expect(modular).to.have.property('registerFolder').that.is.a('function');
+    });
+
+    it('should expose .registerConstant()', function() {
+      expect(modular).to.have.property('registerConstant').that.is.a('function');
+    });
+
+    it('should expose .registerHash()', function() {
+      expect(modular).to.have.property('registerHash').that.is.a('function');
+    });
+
+    it('should expose .register()', function() {
+      expect(modular).to.have.property('register').that.is.a('function');
+    });
+
+    it('should expose .resolve()', function() {
+      expect(modular).to.have.property('resolve').that.is.a('function');
+    });
+
+    it('should expose .remove()', function() {
+      expect(modular).to.have.property('remove').that.is.a('function');
+    });
+
+  });
+
+  describe('API (helper methods)', function() {
+
+    it('should expose .isRegistered()', function() {
+      expect(modular).to.have.property('isRegistered').that.is.a('function');
+    });
+
+    it('should expose .isConstant()', function() {
+      expect(modular).to.have.property('isConstant').that.is.a('function');
+    });
+
+    it('should expose .isFunction()', function() {
+      expect(modular).to.have.property('isFunction').that.is.a('function');
+    });
+
+    it('should expose .isModule()', function() {
+      expect(modular).to.have.property('isModule').that.is.a('function');
+    });
+
+    it('should expose .isInjectable()', function() {
+      expect(modular).to.have.property('isInjectable').that.is.a('function');
+    });
+
+  });
+
+  describe('API (for compatibility with "dependable")', function() {
+
+    it('should expose .load()', function() {
+      expect(modular).to.have.property('load').that.is.a('function');
+    });
+
+    it('should expose .get()', function() {
+      expect(modular).to.have.property('get').that.is.a('function');
+    });
+
+  });
+
+});
