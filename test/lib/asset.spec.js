37a9d3001e6516ee6f83bd7a85f043e00f924202
Johnny Estilles <johnny.estilles@agentia.asia>
2015-04-16 08:16:17 +0800

    refactor(asset): change .is methods to properties

--- test/lib/asset.spec.js
+++ test/lib/asset.spec.js
@@ -21,32 +21,32 @@ describe('Asset()', function() {
   describe('API', function() {
     var asset = new Asset('key', 'value', { constant: true });
 
-    it('should expose .isInjectable()', function() {
-      expect(asset).to.have.property('isInjectable').that.is.a('function');
+    it('should expose .isInjectable', function() {
+      expect(asset).to.have.property('isInjectable');
     });
 
-    it('should expose .isConstant()', function() {
-      expect(asset).to.have.property('isConstant').that.is.a('function');
+    it('should expose .isConstant', function() {
+      expect(asset).to.have.property('isConstant');
     });
 
-    it('should expose .isModule()', function() {
-      expect(asset).to.have.property('isModule').that.is.a('function');
+    it('should expose .isModule', function() {
+      expect(asset).to.have.property('isModule');
     });
 
-    it('should expose .isFunction()', function() {
-      expect(asset).to.have.property('isFunction').that.is.a('function');
+    it('should expose .isFunction', function() {
+      expect(asset).to.have.property('isFunction');
     });
 
-    it('should expose .isResolved()', function() {
-      expect(asset).to.have.property('isResolved').that.is.a('function');
+    it('should expose .isResolved', function() {
+      expect(asset).to.have.property('isResolved');
     });
 
-    it('should expose .getFactory()', function() {
-      expect(asset).to.have.property('getFactory').that.is.a('function');
+    it('should expose .factory', function() {
+      expect(asset).to.have.property('factory');
     });
 
-    it('should expose .getValue()', function() {
-      expect(asset).to.have.property('getValue').that.is.a('function');
+    it('should expose .value', function() {
+      expect(asset).to.have.property('value');
     });
 
     it('should expose .resolveTo()', function() {
@@ -59,15 +59,15 @@ describe('Asset()', function() {
     var asset = new Asset('key', 'value', { constant: true });
 
     it('should return a constant asset', function() {
-      expect(asset.isConstant()).to.be.true;
+      expect(asset.isConstant).to.be.true;
     });
 
     it('should return a resolved asset', function() {
-      expect(asset.isResolved()).to.be.true;
+      expect(asset.isResolved).to.be.true;
     });
 
     it('should allow the retrieval of the asset value', function() {
-      expect(asset.getValue()).to.equal('value');
+      expect(asset.value).to.equal('value');
     });
   });
 
@@ -78,19 +78,19 @@ describe('Asset()', function() {
     var asset = new Asset('key', fn);
 
     it('should return a function asset', function() {
-      expect(asset.isFunction()).to.be.true;
+      expect(asset.isFunction).to.be.true;
     });
 
     it('shoud return an non-injectable asset', function() {
-      expect(asset.isInjectable()).to.be.false;
+      expect(asset.isInjectable).to.be.false;
     });
 
     it('should return a resolved asset', function() {
-      expect(asset.isResolved()).to.be.true;
+      expect(asset.isResolved).to.be.true;
     });
 
     it('should allow the retrieval of the asset value', function() {
-      expect(asset.getValue()).to.equal(fn);
+      expect(asset.value).to.equal(fn);
     });
 
   });
@@ -102,19 +102,19 @@ describe('Asset()', function() {
     var asset = new Asset('key', fn, { injectable: true });
 
     it('should return a function asset', function() {
-      expect(asset.isFunction()).to.be.true;
+      expect(asset.isFunction).to.be.true;
     });
 
     it('shoud return an injectable asset', function() {
-      expect(asset.isInjectable()).to.be.true;
+      expect(asset.isInjectable).to.be.true;
     });
 
     it('should return a non-resolved asset', function() {
-      expect(asset.isResolved()).to.be.false;
+      expect(asset.isResolved).to.be.false;
     });
 
     it('should not allow the retrieval of the asset value', function() {
-      expect(asset.getValue()).to.be.null;
+      expect(asset.value).to.be.null;
     });
 
     it('should have a context of type EventEmitter', function() {
@@ -122,7 +122,7 @@ describe('Asset()', function() {
     });
 
     it('should allow the retrieval of the asset factory', function() {
-      expect(asset.getFactory()).to.equal(fn);
+      expect(asset.factory).to.equal(fn);
     });
 
   });
@@ -135,23 +135,23 @@ describe('Asset()', function() {
     });
 
     it('should return a function asset', function() {
-      expect(asset.isFunction()).to.be.true;
+      expect(asset.isFunction).to.be.true;
     });
 
     it('should return a non-injectable asset', function() {
-      expect(asset.isInjectable()).to.be.false;
+      expect(asset.isInjectable).to.be.false;
     });
 
     it('should return a module asset', function () {
-      expect(asset.isModule()).to.be.true;
+      expect(asset.isModule).to.be.true;
     });
 
     it('should return a resolved asset', function() {
-      expect(asset.isResolved()).to.be.true;
+      expect(asset.isResolved).to.be.true;
     });
 
     it('should allow the retrival of the asset value', function() {
-      expect(asset.getValue()).to.equal(require('../fixtures/test-module-a'));
+      expect(asset.value).to.equal(require('../fixtures/test-module-a'));
     });
 
   });
@@ -164,27 +164,27 @@ describe('Asset()', function() {
     });
 
     it('should return a function asset', function() {
-      expect(asset.isFunction()).to.be.true;
+      expect(asset.isFunction).to.be.true;
     });
 
     it('should return an injectable asset', function() {
-      expect(asset.isInjectable()).to.be.true;
+      expect(asset.isInjectable).to.be.true;
     });
 
     it('should return a module asset', function() {
-      expect(asset.isModule()).to.be.true;
+      expect(asset.isModule).to.be.true;
     });
 
     it('shoud return a non-resolved asset', function() {
-      expect(asset.isResolved()).to.be.false;
+      expect(asset.isResolved).to.be.false;
     });
 
     it('should not allow the retrieval of the asset value', function() {
-      expect(asset.getValue()).to.be.null;
+      expect(asset.value).to.be.null;
     });
 
     it('shoud allow the retrieval of the asset factory', function() {
-      expect(asset.getFactory()).to.equal(require('../fixtures/test-module-a'));
+      expect(asset.factory).to.equal(require('../fixtures/test-module-a'));
     });
 
   });
@@ -198,28 +198,28 @@ describe('Asset()', function() {
     });
 
     it('should return a constant Asset', function() {
-      expect(asset.isConstant()).to.be.true;
+      expect(asset.isConstant).to.be.true;
     });
 
     it('should return an non-injectable Asset', function() {
-      expect(asset.isInjectable()).to.be.false;
+      expect(asset.isInjectable).to.be.false;
     });
 
     it('should return a module Asset', function() {
-      expect(asset.isModule()).to.be.true;
+      expect(asset.isModule).to.be.true;
     });
 
     it('shoud return a resolved Asset', function() {
-      expect(asset.isResolved()).to.be.true;
+      expect(asset.isResolved).to.be.true;
     });
 
     it('should allow the retrieval of the Asset value', function() {
-      expect(asset.getValue())
+      expect(asset.value)
         .to.deep.equal(require('../fixtures/test-module-d'));
     });
 
     it('shoud not have a factory', function() {
-      expect(asset.getFactory()).to.be.null;
+      expect(asset.factory).to.be.null;
     });
 
   });
@@ -233,11 +233,11 @@ describe('Asset()', function() {
     asset.resolveTo(value);
 
     it('should set asset value', function() {
-      expect(asset.getValue()).to.equal(value);
+      expect(asset.value).to.equal(value);
     });
 
     it('should set asset as resolved', function() {
-      expect(asset.isResolved()).to.be.true;
+      expect(asset.isResolved).to.be.true;
     });
 
   });
